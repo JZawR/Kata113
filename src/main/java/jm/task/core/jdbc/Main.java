b4f1605da486ef4b1941ba94a0ed6ef9c63fcb20
Yura Katkov <94790420+JZawR@users.noreply.github.com>
2022-07-17 13:12:22 +0300

    repair like mentor

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,10 +4,9 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        final byte age = 10;
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-        userService.saveUser("Vasya", "Get", age);
+        userService.saveUser("Vasya", "Get", (byte) 50);
         userService.saveUser("Vanya", "Kat", (byte) 52);
         userService.saveUser("Lora", "Get", (byte) 48);
         userService.saveUser("Olya", "Kat", (byte) 47);
